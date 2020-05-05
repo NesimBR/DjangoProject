a61d4d948c6bd067990ef59d421ce5684fe592dc
NesimBR <nesim.khaled@gmail.com>
2020-05-05 06:27:09 +0300

    User panel and profile information

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user.apps.UserConfig',
     'note.apps.NoteConfig',
     'home.apps.HomeConfig',
     'django.contrib.admin',
